a17f7e597a686c6b1d39d1869a8701e9ce4e40f8
Devaka Dias <devaka@villvay.com>
2022-09-06 15:29:55 +0530

    deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,17 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+    const VillvayCert = await ethers.getContractFactory("VillvayCerts");
+    const villvayCert = await VillvayCert.deploy();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+    await villvayCert.deployed();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+    console.log(`contract sucessfully deployed to ${villvayCert.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });

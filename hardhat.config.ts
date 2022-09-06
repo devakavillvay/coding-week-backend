46c4a08c8410be4ec03466811c507fd3a75e05d0
Devaka Dias <devaka@villvay.com>
2022-09-06 15:16:20 +0530

    hardhat config finished

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+    solidity: "0.8.9",
+    networks: {
+        rinkeby: {
+            url: process.env.RINKEBY_URL,
+            accounts: [process.env.RINKEBY_SK as string],
+        },
+    },
 };
 
 export default config;
